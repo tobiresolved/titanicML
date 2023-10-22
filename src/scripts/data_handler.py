236f063b3647abd4a0e0ddc6739df62678c16df6
tobiresolved <ulkmv@student.kit.edu>
2023-10-22 13:14:08 +0200

    cleaning data from missing values

--- src/scripts/data_handler.py
+++ src/scripts/data_handler.py
@@ -6,13 +6,22 @@ class Data_Handling():
     def __init__(self):
         self.train_data = self.read_csv_into_dataframe("src\\data\\train.csv")
         self.test_data = self.read_csv_into_dataframe("src\\data\\test.csv")
-        self.explore_data(self.train_data)
-        
-        self.train_data = self.clean_data(self.train_data)
-        self.test_data = self.clean_data(self.test_data)
-        self.explore_data(self.train_data)
+        self.concat_df()
+        self.explore_data()
+        self.clean_data()
+        self.explore_data()
+        self.divide_df()
         self.split_into_X_y()
-        self.split_into_test_validation_sets()
+
+    def concat_df(self):
+        # Returns a concatenated df of training and test set
+        self.all_data =  pd.concat([self.train_data, self.test_data], sort=True).reset_index(drop=True)
+    
+    def divide_df(self):
+        # Returns divided dfs of training and test set
+        self.train_data = self.all_data.loc[:890]
+        self.test_data = self.all_data.loc[891:].drop(['Survived'], axis=1)
+       
 
     def read_csv_into_dataframe(self, link):
         # Reads a csv into pandas dataframa
@@ -28,26 +37,43 @@ class Data_Handling():
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
     
-    def explore_data(self, dataframe):
-        print("____________________________________________________")
-        print(dataframe.head())
-        print(dataframe.describe())
-        print(dataframe.isnull().sum())
-        print(dataframe.isnull())
-        print(dataframe.isna())
-        columns_with_null = dataframe.columns[dataframe.isna().any()]
-        print(columns_with_null)
-        print("____________________________________________________")
+    def explore_data(self):
+        try:
+            self.all_data.info()
+            columns_with_null = self.all_data.columns[self.all_data.isna().any()]
+            print(f"COLLUMNS WITH MISSING VALUES: {columns_with_null}")
+        except Exception as e:
+            print(f"An unexpected error occurred during exploring: {e}")
+        
+    def fill_age(self):
+        try:
+            median_age_by_pclass_sex = self.all_data.groupby(['Sex', 'Pclass'])['Age'].transform('median')
+            self.all_data['Age'].fillna(median_age_by_pclass_sex, inplace=True)
+        except Exception as e:
+            print(f"An error occurred: {e}. Please review your data for potential issues.")
+
+    def fill_fare(self):
+        try:
+            med_fare = self.all_data.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
+            self.all_data['Fare'] = self.all_data['Fare'].fillna(med_fare)
+        except Exception as e:
+            print(f"An error occurred: {e}. Please review your data for potential issues.")
+       
+    
 
 
-
-    def clean_data(self, dataframe):
+    def clean_data(self):
         # Cleans the data and handles missing data
         try:
-            dataframe = dataframe.drop(columns=['Cabin']) # Drop the Cabin attribute due to too many missing values
+            # Fill the Age attribute by the median of passenger class and sex
+            self.fill_age()
+            # Fill the two missing ports
+            self.all_data['Embarked'] = self.all_data['Embarked'].fillna('S') 
+            # Fill the missing fare by the median of third class with no family aboard
+            self.fill_fare()
+            # Drop the Cabin attribute due to too many missing values
+            self.all_data = self.all_data.drop(columns=['Cabin'])
 
-            
-            return dataframe
         except Exception as e:
             print(f"An error occurred during data cleaning: {e}")
 
@@ -59,6 +85,7 @@ class Data_Handling():
         except KeyError as e:
             print(f"An error occurred: {e}")
             print("The specified column does not exist in the DataFrame.")
+
     def split_into_test_validation_sets(self):
         # Splits the feature set "X" into a train and a validation set
         try:
--- src/scripts/main.py
+++ src/scripts/main.py
@@ -6,34 +6,35 @@ def main():
 
     dh = Data_Handling()
     clf = CustomLogisticRegression(data_handler= dh)
+    clf.train(dh.X, dh.y)
+
+    y_prediction = clf.predict(dh.test_data)
+
+    print(y_prediction)
+
+    submission = pd.read_csv("src\\data\\submission.csv")
+    submission['Survived'] = y_prediction
+    submission.to_csv("src\\data\\submission.csv", index=False)
+
   
     if False:
         clf.train(df.X, df.y)
-        # Make predictions
         y_pred = clf.predict(df.X_val)
-
-        # Evaluate the model's accuracy
         accuracy = accuracy_score(df.y_val, y_pred)
         print(f"Accuracy: {accuracy * 100:.2f}%")
-
         print(df.test_data)
         missing_values = df.test_data.isna()  # or df.isnull()
-
-# You can also count the missing values in each column
         missing_counts = df.test_data.isna().sum()  # or df.isnull().sum()
         print(df.test_data)
-
-    
-
         print("\nMissing Value Counts per Column:")
         print(missing_counts)
 
         y_prediction = clf.predict(df.test_data)
         print(y_prediction)
 
-        submission = pd.read_csv("src\\data\\gender_submission.csv")
+        submission = pd.read_csv("src\\data\\submission.csv")
         submission['Survived'] = y_prediction
-        submission.to_csv("src\\data\\gender_submission.csv", index=False)
+        submission.to_csv("src\\data\\submission.csv", index=False)
 
 
 
